10149bda9807d5ed6fd3190416162e230c1cfd9d
Akash Tiwari <akashtiwari722003@gmail.com>
2024-04-05 04:19:11 +0530

    initial_commit

---
+++ src/main/java/com/akash/springbootappdemo/springbootappdemo/SpringbootappdemoApplication.java
@@ -0,0 +1,13 @@
+package com.akash.springbootappdemo.springbootappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootappdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootappdemoApplication.class, args);
+	}
+
+}
