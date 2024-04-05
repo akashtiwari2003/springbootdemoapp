41f5474d4a56a939808a108714391338a469a4fe
Akash Tiwari <akashtiwari722003@gmail.com>
2024-04-05 10:56:03 +0530

    service_and_dto_setup

---
+++ src/main/java/com/akash/springbootappdemo/springbootappdemo/Dto/AccountDto.java
@@ -0,0 +1,7 @@
+package com.akash.springbootappdemo.springbootappdemo.Dto;
+
+public class AccountDto {
+    private Long id;
+    private String accountHolderName;
+    private double balance;
+}
---
+++ src/main/java/com/akash/springbootappdemo/springbootappdemo/service/AccountService.java
@@ -0,0 +1,8 @@
+package com.akash.springbootappdemo.springbootappdemo.service;
+
+import com.akash.springbootappdemo.springbootappdemo.Dto.AccountDto;
+
+
+public interface AccountService {
+    AccountDto createAccount(AccountDto accountDto);
+}
