e7d7d50eb658589cc840f53a0a526440f2ec77f3
Akash Tiwari <akashtiwari722003@gmail.com>
2024-04-05 13:21:17 +0530

    service_implementation_create_account

--- src/main/java/com/akash/springbootappdemo/springbootappdemo/service/impl/AccountServiceImpl.java
+++ src/main/java/com/akash/springbootappdemo/springbootappdemo/service/impl/AccountServiceImpl.java
@@ -1,6 +1,8 @@
 package com.akash.springbootappdemo.springbootappdemo.service.impl;
 
 import com.akash.springbootappdemo.springbootappdemo.Dto.AccountDto;
+import com.akash.springbootappdemo.springbootappdemo.entity.Account;
+import com.akash.springbootappdemo.springbootappdemo.mapper.AccountMapper;
 import com.akash.springbootappdemo.springbootappdemo.repository.AccountRepository;
 import com.akash.springbootappdemo.springbootappdemo.service.AccountService;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,8 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public AccountDto createAccount(AccountDto accountDto) {
-        return null;
+        Account account = AccountMapper.mapToAccount(accountDto);
+        Account savedAccount = accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(savedAccount);
     }
 }
